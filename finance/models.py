5a2e2d5269f14eb01bd8e03b7fef84431a4ee0a5
Gyabaa Yeboah Emmanuel <gyabaayeboahemmanuel@gmail.com>
2021-10-14 19:23:33 -0700

    Fundational Models Created

--- finance/models.py
+++ finance/models.py
@@ -2,16 +2,8 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
-# Create your models here.
-class MachineWork (models.Model):
-    description = models.CharField(max_length=200)
-    entry_date = models.DateTimeField(default=timezone.now)
-    amount = models.DecimalField(decimal_places=2, max_digits=9)
 
-    def __str__(self):
-        return self.description
-    def was_entered_today(self):
-        return self.entry_date >= timezone.now() - datetime.timedelta
+# Models For people
 
 class Operator (models.Model): 
     name = models.CharField(max_length=100)
@@ -30,4 +22,39 @@ class Staff(models.Model):
 
     def __str__(self) -> str:
         return self.surname + " " + self.otherNames
+
  
+
+# Models for Works
+class MachineWork (models.Model):
+    description = models.CharField(max_length=200)
+    entry_date = models.DateTimeField(default=timezone.now)
+    amount = models.DecimalField(decimal_places=2, max_digits=9)
+
+    def __str__(self):
+        return self.description
+    def was_entered_today(self):
+        return self.entry_date >= timezone.now() - datetime.timedelta
+
+class Capentry(models.Model):
+    date = models.DateTimeField(default=timezone.now)
+    description = models.CharField(max_length=200)
+    unit_price = models.DecimalField(decimal_places=2, max_digits=9)
+    quantity = models.IntegerField()
+    expenses = models.DecimalField(decimal_places=2, max_digits=9)
+
+class WoodFromBush(models.Model):
+    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    woodtype = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+
+
+class WoodWork(models.Model):
+    date = models.DateTimeField(default=timezone.now)
+    description = models.CharField(max_length=200)
+    unit_price = models.DecimalField(decimal_places=2, max_digits=9)
+    quantity = models.IntegerField()
+    amount = models.DecimalField(decimal_places=2, max_digits=9)
+
+
--- finance/urls.py
+++ finance/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
 
-urlpattens = [
+urlpatterns = [
     path('machinework', views.machinework, name='machinework'),
 ]
