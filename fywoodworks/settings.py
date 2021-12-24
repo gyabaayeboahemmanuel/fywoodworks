b84f0e738d62d59fc86c0e683df2c1611f12ee25
prince-cloud <acheampongprince227@gmail.com>
2021-12-24 03:18:48 +0000

    added bootstrap app

--- fywoodworks/settings.py
+++ fywoodworks/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'crispy_forms',
+    'bootstrap',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
